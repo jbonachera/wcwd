fc7dd9adb7b72b9c41ad6be0ba8f8e0b759915a9
Julien BONACHERA <julien.bonachera@fortuneo.com>
2020-09-21 16:51:26 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/jbonachera/wcwd
+
+go 1.15
+
+require (
+	github.com/Difrex/gosway v0.0.0-20200404094735-5bd39542181e
+	go.i3wm.org/i3/v4 v4.18.0
+)
---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/Difrex/gosway/ipc"
+)
+
+func findFocused(nodes []Node) (Node, bool) {
+	for _, node := range nodes {
+		if node.Focused {
+			return node, true
+		}
+		n, ok := findFocused(node.Nodes)
+		if ok {
+			return n, ok
+		}
+	}
+	return Node{}, false
+}
+
+type Node struct {
+	Nodes         []Node `json:"nodes,omitempty"`
+	FloatingNodes []Node `json:"floating_nodes,omitempty"`
+	Pid           int    `json:"pid,omitempty"`
+	Focused       bool   `json:"focused"`
+	Focus         []int  `json:"focus"`
+}
+
+type Tree struct {
+	Name  string `json:"name"`
+	Nodes []Node `json:"nodes"`
+}
+
+func getTree() (*Tree, error) {
+	sc, err := ipc.NewSwayConnection()
+	if err != nil {
+		return nil, err
+	}
+
+	tree := &Tree{}
+
+	data, err := sc.SendCommand(ipc.IPC_GET_TREE, "get_tree")
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(data, tree)
+	return tree, err
+}
+
+func findFocusedPID() (int, error) {
+	tree, err := getTree()
+	if err != nil {
+		log.Fatal(err)
+	}
+	node, ok := findFocused(tree.Nodes)
+	if ok {
+		return node.Pid, nil
+	}
+	return 0, errors.New("pid not found")
+}
+
+func main() {
+	pid, err := findFocusedPID()
+	if err != nil {
+		fmt.Println(os.Getenv("HOME"))
+	}
+	path, err := os.Readlink(fmt.Sprintf("/proc/%d/cwd", pid))
+	if err != nil {
+		fmt.Println(os.Getenv("HOME"))
+	}
+	fmt.Println(path)
+}
